4394ae91cf458648f5ef23482cfefb6af64e301a
Jared Abraham Bard <jbard@midway3-login2.rcc.local>
2022-03-06 13:55:15 -0600

    updated script

--- scripts/analysis/bin_counts_100_python_input.R
+++ scripts/analysis/bin_counts_100_python_input.R
@@ -1,22 +1,32 @@
 library(tidyverse)
-# working.dir <- "/home/jbard/scratch-midway2/TSP"
-# gene_labels <- read_tsv(paste0(working.dir,"/Analysis/src/201014_labeled_genes_scer.txt")) %>%
-#   mutate(length.trans=length.prot*3)
-# 
-# files <- list.files(paste0(working.dir,"/big/hisat2/210614/counts"),
-#                           pattern="*.tsv.gz",full.names = T)
-gene_labels <- read_tsv("src/201014_labeled_genes_scer.txt") %>%
-     mutate(length.trans=length.prot*3)
-files <- list.files("../big/counts",pattern="*.tsv.gz",full.names=T)
-names <- str_extract(files,pattern="(?<=_)S[0-9]*(?=_counts\\.tsv.gz)")
+working.dir <- "/home/jbard/beagle3-dadrummond/jbard/211216/snake"
+gene_labels <- read_tsv(paste0(working.dir,"/src/201014_labeled_genes_scer.txt")) %>%
+  mutate(length.CDS=length.prot*3)
+
+files <- list.files(paste0(working.dir,"/counts"),
+                          pattern="dist.tsv",full.names = T,recursive=TRUE)
+
+names <- str_extract(files,pattern="(?<=211216_STAR_).*(?=_dist\\.tsv)")
 t <- tibble(names,files)
 
 t$files %>% map2(t$names,function(file,name) {
-  d_raw <- read_tsv(file) %>%
-    left_join(gene_labels %>% select(ORF,length.trans),by="ORF") %>%
-    rename("Counts"="Count") %>%
-    filter(!is.na(length.trans))
+  d_raw0 <- read_tsv(file,comment="#") %>%
+    left_join(gene_labels %>% select(ORF,length.CDS),by="ORF") %>%
+    rename("Counts"="Count",
+           "Pos"="index") %>%
+    filter(!is.na(length.CDS))
   
+  # find min and max
+  d_length <- d_raw0 %>% select(Pos,ORF,length.CDS) %>%
+    group_by(ORF,length.CDS) %>% nest %>%
+    mutate(start = map_dbl(data,function(df){min(df$Pos)}),
+           end = map_dbl(data,function(df){max(df$Pos)})) %>%
+    select(-data) %>% ungroup %>%
+    mutate(start=if_else(start>-10,-10,start),
+           end=if_else(end<(length.CDS+9),length.CDS+9,end),
+           length.trans=end-start) #expand transcripts to at least the CDS length
+  d_raw <- d_raw0 %>%
+    left_join(d_length %>% select(ORF,length.trans),by="ORF")
   total.RPK <- d_raw %>% group_by(ORF,length.trans) %>%
       summarise(counts.perORF = sum(Counts,na.rm=T)) %>%
       ungroup() %>%
@@ -25,7 +35,7 @@ t$files %>% map2(t$names,function(file,name) {
   
   tpms <- d_raw %>% group_by(ORF,length.trans) %>%
       summarise(counts.perORF = sum(Counts,na.rm=T),.groups="drop") %>%
-    ungroup %>%
+      ungroup %>%
       mutate(RPK = counts.perORF / (length.trans/1000),
              TPM = RPK*1e6/total.RPK)
 
@@ -39,30 +49,31 @@ t$files %>% map2(t$names,function(file,name) {
       mutate(start_aligned = Pos) %>%
       select(start_aligned,ORF,counts.norm)
   
-  bin_orf <- function(df,N) {
-    bin_vector <- as.integer(cut_interval(df$start_aligned,n=N,labels=seq(1,N)))
-    df %>% add_column(bin_vector)
+  bin_orf <- function(df,N,vector) {
+    bin_vector <- as.integer(cut_interval(vector,n=N,labels=seq(1,N)))
+    bin_df <- tibble("start_aligned"=vector,"bin_vector"=bin_vector)
+    df %>% left_join(bin_df,by="start_aligned")
   }
   
-  bin_df <- function(df,N,bin) {
+  bin_df <- function(df,bin) {
     if (bin=="UTR5") {
-      df %>% group_by(ORF) %>%
+      df %>% group_by(ORF,start,end,length.CDS) %>%
         nest %>%
-        mutate(data=map(data,bin_orf,N)) %>%
+        mutate(data=map(data,bin_orf,10,seq(start,-1))) %>%
         unnest(data) %>%
-        mutate(bin_vector = bin_vector-26) %>%
+        mutate(bin_vector = bin_vector-10) %>%
         ungroup() %>% return
     } else if (bin=="ORF") {
-      df %>% group_by(ORF) %>%
+      df %>% group_by(ORF,start,end,length.CDS) %>%
         nest %>%
-        mutate(data=map(data,bin_orf,N)) %>%
+        mutate(data=map(data,bin_orf,100,seq(0,length.CDS-1))) %>%
         unnest(data) %>%
         mutate(bin_vector = bin_vector-1) %>%
         ungroup() %>% return
     } else if (bin=="UTR3") {
-      df %>% group_by(ORF) %>%
+      df %>% group_by(ORF,start,end,length.CDS) %>%
         nest %>%
-        mutate(data=map(data,bin_orf,N)) %>%
+        mutate(data=map(data,bin_orf,10,seq(length.CDS,end))) %>%
         unnest(data) %>%
         mutate(bin_vector = bin_vector+99) %>%
         ungroup() %>% return
@@ -70,15 +81,15 @@ t$files %>% map2(t$names,function(file,name) {
   }
   
   d_counts_binned <- d_normcounts %>%
-    left_join(gene_labels %>% select(ORF,length.trans),by="ORF") %>%
+    left_join(d_length,by="ORF") %>%
     mutate(bin = case_when(start_aligned<0~"UTR5",
-                           start_aligned>=0&start_aligned<length.trans~"ORF",
-                           start_aligned>=length.trans~"UTR3")) %>%
+                           start_aligned>=0&start_aligned<length.CDS~"ORF",
+                           start_aligned>=length.CDS~"UTR3")) %>%
     group_by(bin) %>%
     nest %>%
-    mutate(data = case_when(bin=="UTR5"~map(data,bin_df,N=25,bin),
-                            bin=="ORF"~map(data,bin_df,N=100,bin),
-                            bin=="UTR3"~map(data,bin_df,N=25,bin),
+    mutate(data = case_when(bin=="UTR5"~map(data,bin_df,bin),
+                            bin=="ORF"~map(data,bin_df,bin),
+                            bin=="UTR3"~map(data,bin_df,bin),
                             TRUE ~ data)) %>%
     unnest(data) %>%
     ungroup %>%
@@ -88,7 +99,7 @@ t$files %>% map2(t$names,function(file,name) {
   d_binned_byORF <- d_counts_binned %>%
     group_by(ORF,bin) %>%
         summarise(counts = sum(counts.norm,na.rm=T)) %>%
-    left_join(tpms %>% rename("TPM.hisat2" = "TPM") %>% select(ORF,TPM.hisat2),by=c("ORF")) %>%
+    left_join(tpms %>% rename("TPM.STAR" = "TPM") %>% select(ORF,TPM.STAR),by=c("ORF")) %>%
     #write_tsv(paste0(working.dir,"/Analysis/output/bin100/counts_bin100_",name,".tsv.gz"))
-    write_tsv(paste0("output/bin100/python/201706_TSP_counts_bin100_",name,".tsv.gz"))
+    write_tsv(paste0("counts/",name,"/211216_counts_bin100_",name,".tsv.gz"))
 })
