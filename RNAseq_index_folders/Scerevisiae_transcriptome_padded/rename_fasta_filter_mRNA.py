4d5f09e2b6fc144cf1f65ab48c886e973933c210
Jared Bard <jabard89@gmail.com>
2023-04-13 11:50:27 -0500

    dont add headers to fasta files

--- RNAseq_index_folders/Scerevisiae_transcriptome_padded/rename_fasta_filter_mRNA.py
+++ RNAseq_index_folders/Scerevisiae_transcriptome_padded/rename_fasta_filter_mRNA.py
@@ -2,12 +2,8 @@
 # Date: 4/13/2023
 # uses Biopython to rename fasta sequences to just the ORF name and filter for mRNA
 
-import sys, os, math, random, argparse, csv
-from datetime import datetime
-import pandas as pd
-import numpy as np
+import sys, os, math, random, argparse, csv, re
 from Bio import SeqIO
-import re
 
 if __name__ == '__main__':
 	parser = argparse.ArgumentParser(description="Generate options")
@@ -17,26 +13,6 @@ if __name__ == '__main__':
 	#args = parser.parse_args(["/home/jabard89/Dropbox/code_JB/repos/rnaseq_tools/RNAseq_index_folders/"
 	# 						"Scerevisiae_transcriptome_padded/Saccharomyces_cerevisiae.R64-1-1_transcriptome_padded_600up_30down.fasta",""])
 	args = parser.parse_args()
-
-	#Read in comments from original file
-	header = []
-	with open(args.in_file,'r') as f:
-		for line in f:
-			if line[0] != "#":
-				break
-			header.append(line)
-
-	# Write out parameters
-	with open(args.out_file,'w') as f:
-		f.write("# Run started {}\n".format(datetime.now()))
-		f.write("# Command: {}\n".format(' '.join(sys.argv)))
-		f.write("# Parameters:\n")
-		optdict = vars(args)
-		for (k,v) in optdict.items():
-			f.write("#\t{k}: {v}\n".format(k=k,v=v))
-
-		for line in header:
-			f.write(line)
 
 	# input fastas
 	input_fasta=list(SeqIO.parse(args.in_file,format='fasta'))
@@ -51,5 +27,5 @@ if __name__ == '__main__':
 			continue
 		record.id = ORF
 		record.name =ORF
-	with open(args.out_file,'a') as f:
+	with open(args.out_file,'w') as f:
 		SeqIO.write(output_fasta,f,"fasta")
--- RNAseq_index_folders/Scerevisiae_transcriptome_padded/trim_fasta.py
+++ RNAseq_index_folders/Scerevisiae_transcriptome_padded/trim_fasta.py
@@ -1,7 +1,4 @@
 import sys, os, math, random, argparse, csv
-from datetime import datetime
-import pandas as pd
-import numpy as np
 from Bio import SeqIO
 
 if __name__ == '__main__':
@@ -14,26 +11,6 @@ if __name__ == '__main__':
 	parser.add_argument("-m","--min_length",dest="min_length",default=100,type=int,help="minimum remaining nt")
 	args = parser.parse_args()
 
-	#Read in comments from original file
-	header = []
-	with open(args.in_file,'r') as f:
-		for line in f:
-			if line[0] != "#":
-				break
-			header.append(line)
-
-	# Write out parameters
-	with open(args.out_file,'w') as f:
-		f.write("# Run started {}\n".format(datetime.now()))
-		f.write("# Command: {}\n".format(' '.join(sys.argv)))
-		f.write("# Parameters:\n")
-		optdict = vars(args)
-		for (k,v) in optdict.items():
-			f.write("#\t{k}: {v}\n".format(k=k,v=v))
-
-		for line in header:
-			f.write(line)
-
 	# input and trim fastas
 	input_fasta=list(SeqIO.parse(args.in_file,format='fasta'))
 	output_fasta=[record for record in input_fasta if len(record)>(args.trim_front+args.trim_back+args.min_length)]
@@ -41,5 +18,5 @@ if __name__ == '__main__':
 		output_fasta=[record[args.trim_front:] for record in output_fasta]
 	else:
 		output_fasta=[record[args.trim_front:-args.trim_back] for record in output_fasta]
-	with open(args.out_file,'a') as f:
+	with open(args.out_file,'w') as f:
 		SeqIO.write(output_fasta,f,"fasta")
